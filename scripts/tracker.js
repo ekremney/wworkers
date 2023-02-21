e00447c5d6c754fa7c675d7b995bb92b8019bef0
ekdogan <ekdogan@adobe.com>
2023-02-21 13:02:41 +0100

    tracker src

---
+++ scripts/tracker.js
@@ -0,0 +1,8 @@
+console.log('TRACKER IS ON');
+
+const div = document.createElement('div');
+div.innerHTML = 'ADDED BY TRACKER';
+
+window.document.body.prepend(div);
+
+console.log('TRACKER IS DONE');
